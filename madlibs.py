b9177ddb373018a18dfe0a74b986ccfababefa08
cam <camrongranger@gmail.com>
2026-05-22 23:18:34 -0400

    Add files via upload

---
+++ madlibs.py
@@ -0,0 +1,12 @@
+# Mad Libs Game
+
+adjective1 = input("Enter an adjective: ")
+noun1 = input("Enter a noun: ")
+adjective2 = input("Enter another adjective: ")
+verb1 = input("Enter a verb ending in -ing: ")
+adjective3 = input("Enter one more adjective: ")
+
+print(f"Today I went to a {adjective1} zoo.")
+print(f"In an exhibit I saw a {noun1}.")
+print(f"The {noun1} was {adjective2} and {verb1}.")
+print(f"I was {adjective3}!")
